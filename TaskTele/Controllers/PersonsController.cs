3142e492e136d5a291bcce196f6c815c45ef6362
Nikita <e4132dm@gmail.com>
2022-01-15 19:02:21 +0300

    1

--- TaskTele/Controllers/PersonsController.cs
+++ TaskTele/Controllers/PersonsController.cs
@@ -25,7 +25,6 @@ namespace TaskTele.Controllers
         public async Task<IActionResult> List (string sex, string x, string y, int page = 1) {
             int pageSize = 4;   // количество элементов на странице
 
-
             IEnumerable<Person> person = null;
             bool result = int.TryParse(sex, out var number);
             if (result == true) {
@@ -41,34 +40,25 @@ namespace TaskTele.Controllers
                 switch (sex) {
                     case "male":
                         person = _person.Persons.Where(i => i.sex == true);
-                        foreach(var age in person) {
-                            age.age = 0;
-                        }
-
                         break;
                     case "female":
                         person = _person.Persons.Where(i => i.sex == false);
-
-                        foreach (var per in person) {
-                            per.age = 0;
-                        }
-
                         break;
                 }
 
                 if (!string.IsNullOrEmpty(x) & !string.IsNullOrEmpty(y)) {
                     //  try {
                     person = person.Where(i => (i.age > Convert.ToInt32(x)) && (i.age < Convert.ToInt32(y))).OrderBy(i => i.age).ToList();
-                    foreach (var per in person) {
-                        per.age = 0;
-                    }
+                   
                     //} catch(FormatException e) {
                     //    x = "0";
                     //    y = "100";
                     //    person = person.OrderBy(i => i.age);
                 }
+                foreach (var per in person) {
+                    per.age = 0;
+                }
 
-                
             }
 
             IQueryable<Person> source = person.AsQueryable();
